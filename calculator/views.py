959ec99f606117ae4138132bc8560ef01e1b521b
Pakkapong <s6101012630119@email.kmutnb.ac.th>
2020-02-26 22:40:33 +0700

    create all function, can calculate and show result

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#def home(request):
+#    return render(request, 'home.html')
+
+def calculator(request):
+    if request.POST.get('x') and request.POST.get('y'):
+        x = float(request.POST['x'])
+        y = float(request.POST['y'])
+        if request.POST.get('add'):
+            result = x + y
+        elif request.POST.get('sub'):
+            result = x - y
+        elif request.POST.get('mul'):
+            result = x * y
+        elif request.POST.get('div'):
+            result = x / y
+        return render(request , "home.html",{'result' : result} )
+    return render(request , "home.html")
